494ef7d40a9bedb0d3314010880bf28c52cb21ac
Robert Górnicki <github@gorki.net.pl>
2022-01-24 04:06:18 +0100

    Work around issue with anchor links
    NeoReader could not open internal links that had two digits next to each other. Reported them as external

--- src/domain/navigation.ts
+++ src/domain/navigation.ts
@@ -3,6 +3,21 @@ import { AnchorDayTypes, AnchorMonthTypes } from '../types/common';
 
 type AnchorIdType = 'cover' | 'calendar' | 'month' | 'day' | 'daySchedule' | 'dayNotes';
 
+export const replaceDigitsWithWords = (str: string) => {
+	let replaced = str.replace(/0/g, 'zero');
+	replaced = replaced.replace(/1/g, 'one');
+	replaced = replaced.replace(/2/g, 'two');
+	replaced = replaced.replace(/3/g, 'three');
+	replaced = replaced.replace(/4/g, 'four');
+	replaced = replaced.replace(/5/g, 'five');
+	replaced = replaced.replace(/6/g, 'six');
+	replaced = replaced.replace(/7/g, 'seven');
+	replaced = replaced.replace(/8/g, 'eight');
+	replaced = replaced.replace(/9/g, 'nine');
+
+	return replaced;
+}
+
 export const getAnchorId = (type: AnchorIdType, monthIndex?: number, dayIndex?: number) => {
 	if (type === 'cover') {
 		return anchorsKeys.cover;
